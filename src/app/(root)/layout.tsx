a97a19d0d619655cdc28c83e0a74e9cb276dd746
zabe-dev <jay@zabe.dev>
2025-08-25 18:08:47 +0800

    chore: redo app structure

--- src/app/(root)/layout.tsx
+++
@@ -1,9 +0,0 @@
-export default async function RootLayout({
-	children,
-}: Readonly<{ children: React.ReactNode }>) {
-	return (
-		<div className="relative flex min-h-svh flex-col">
-			<main className="flex flex-1 flex-col">{children}</main>
-		</div>
-	);
-}
