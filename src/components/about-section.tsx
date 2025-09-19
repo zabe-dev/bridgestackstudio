33535baca775b09f61b4376949d4465b58fbc8a0
zabe-dev <jay@zabe.dev>
2025-09-20 03:58:09 +0800

    refactor: update services section

--- src/components/about-section.tsx
+++ src/components/about-section.tsx
@@ -8,30 +8,31 @@ import RevealOnScroll from "./reveal-on-scroll";
 
 export default function AboutSection() {
 	return (
-		<section
-			id="about"
-			className="mx-auto flex max-w-[1400px] flex-col-reverse px-8 py-12 md:grid md:grid-cols-2 md:py-48"
-		>
-			<div className="relative">
-				<motion.div
-					animate={{ y: [0, -15, 0] }}
-					transition={{
-						duration: 4,
-						repeat: Infinity,
-						ease: "easeInOut",
-					}}
-					className="relative top-0 mx-auto mt-12 md:mt-0"
-				>
-					<Image
-						src="/about-section-image.svg"
-						alt="Hero Image"
-						width={550}
-						height={550}
-						priority
-					/>
-				</motion.div>
-			</div>
-			<RevealOnScroll delay={0.5}>
+		<RevealOnScroll delay={0.5}>
+			<section
+				id="about"
+				className="mx-auto flex max-w-[1400px] flex-col-reverse px-8 py-12 md:grid md:grid-cols-2 md:py-48"
+			>
+				<div className="relative">
+					<motion.div
+						animate={{ y: [0, -15, 0] }}
+						transition={{
+							duration: 4,
+							repeat: Infinity,
+							ease: "easeInOut",
+						}}
+						className="relative top-0 mx-auto mt-12 md:mt-0"
+					>
+						<Image
+							src="/about-section-image.svg"
+							alt="Hero Image"
+							width={550}
+							height={550}
+							priority
+						/>
+					</motion.div>
+				</div>
+
 				<div className="flex flex-col gap-6 py-2 md:gap-8">
 					<div className="flex flex-wrap gap-2">
 						<p className="text-foreground/80 text-xs font-bold tracking-widest uppercase md:text-sm">
@@ -69,7 +70,7 @@ export default function AboutSection() {
 						</Link>
 					</div>
 				</div>
-			</RevealOnScroll>
-		</section>
+			</section>
+		</RevealOnScroll>
 	);
 }
--- src/components/services-section.tsx
+++ src/components/services-section.tsx
@@ -2,6 +2,10 @@
 
 import {
 	IconBolt,
+	IconBrandFramerMotion,
+	IconBrandNextjs,
+	IconBrandReact,
+	IconBrandTailwind,
 	IconDeviceMobile,
 	IconPalette,
 	IconSearch,
@@ -11,43 +15,50 @@ import {
 import { motion } from "motion/react";
 import RevealOnScroll from "./reveal-on-scroll";
 
-export const services = [
+const services = [
 	{
 		title: "Custom Coded",
 		description:
 			"We create websites that reflect your brand identity and engage your audience. Every design is unique and tailored to your business goals.",
-		icon: <IconPalette />,
+		icon: IconPalette,
 	},
 	{
 		title: "Responsive Design",
 		description:
 			"Your website will look and perform flawlessly on all devices, from desktops to smartphones. We prioritize a smooth experience for every visitor.",
-		icon: <IconDeviceMobile />,
+		icon: IconDeviceMobile,
 	},
 	{
 		title: "100% Page Speed",
 		description:
 			"Our sites are optimized for speed, reliability, and seamless navigation. Fast-loading websites keep users engaged and reduce bounce rates.",
-		icon: <IconBolt />,
+		icon: IconBolt,
 	},
 	{
 		title: "SEO & Digital Marketing",
 		description:
 			"We implement clear, effective SEO strategies to improve your search rankings. Our approach drives traffic and helps grow your online presence.",
-		icon: <IconSearch />,
+		icon: IconSearch,
 	},
 	{
 		title: "Unmatched Support",
 		description:
 			"We provide reliable support to keep your website secure, updated, and performing at its best. You’ll have peace of mind knowing help is always available.",
-		icon: <IconTools />,
+		icon: IconTools,
 	},
 	{
 		title: "Money-back Guarantee",
 		description:
-			"We build online stores and landing pages designed to maximize sales and leads. Every element is strategically placed to boost conversions.",
-		icon: <IconShield />,
+			"If our design doesn’t meet your expectations, you’ll receive a full refund and the contract will be canceled. We proudly stand behind the quality of our work.",
+		icon: IconShield,
 	},
+];
+
+const techIcons = [
+	{ icon: IconBrandReact, label: "ReactJS" },
+	{ icon: IconBrandNextjs, label: "NextJS" },
+	{ icon: IconBrandTailwind, label: "TailwindCSS" },
+	{ icon: IconBrandFramerMotion, label: "Motion" },
 ];
 
 export default function ServicesSection() {
@@ -59,12 +70,11 @@ export default function ServicesSection() {
 			<RevealOnScroll delay={0.2}>
 				<div className="mx-auto flex max-w-5xl flex-col gap-8 py-2">
 					<p className="text-foreground/80 text-xs font-bold tracking-widest uppercase md:text-sm">
-						BUILT FOR SMALL LOCAL BUSINESSES
+						EMPOWERING SMALL & LOCAL BUSINESSES
 						<span className="text-primary">.</span>
 					</p>
 					<p className="leading-tightest text-4xl font-black md:text-6xl">
-						We <span className="text-primary">build</span>,{" "}
-						<span className="text-primary">host</span> and{" "}
+						We <span className="text-primary">build, host</span> and{" "}
 						<span className="text-primary">maintain</span> your website so you
 						can focus on your business.
 					</p>
@@ -73,11 +83,41 @@ export default function ServicesSection() {
 						edits, 24/7 support, and lifetime updates, all with a 12-month
 						minimum commitment.
 					</p>
+
+					<div className="flex flex-col gap-4 py-12">
+						<p className="text-foreground/95 leading-7 font-light md:text-sm md:leading-8">
+							BUILT WITH TRUSTED TECHNOLOGIES
+						</p>
+						<div className="flex items-center gap-4">
+							{techIcons.map(({ icon: Icon, label }, index) => (
+								<motion.div
+									key={label}
+									initial={{ opacity: 0, y: -20 }}
+									whileInView={{ opacity: 1, y: 0 }}
+									viewport={{ once: true }}
+									transition={{
+										delay: index * 0.2,
+										duration: 0.5,
+										ease: "easeOut",
+									}}
+									className="flex items-center gap-2"
+								>
+									<Icon
+										stroke={1}
+										className="text-primary h-10 w-10 transition-colors duration-500"
+									/>
+									<span className="hidden text-sm md:inline-block">
+										{label}
+									</span>
+								</motion.div>
+							))}
+						</div>
+					</div>
 				</div>
 			</RevealOnScroll>
 
-			<div className="grid gap-8 md:px-6 lg:grid-cols-2">
-				{services.map(({ title, description, icon }, index) => (
+			<div className="grid gap-8 py-12 md:px-6 lg:grid-cols-2">
+				{services.map(({ title, description, icon: Icon }, index) => (
 					<RevealOnScroll key={title} delay={index * 0.1}>
 						<motion.div
 							className="flex flex-col gap-0.5"
@@ -86,10 +126,15 @@ export default function ServicesSection() {
 							viewport={{ once: true }}
 							transition={{ duration: 0.5, ease: "easeOut" }}
 						>
-							<h3 className="mb-2 flex items-center gap-4 text-xl font-medium uppercase">
-								<span className="text-primary">{icon}</span> {title}
+							<h3 className="mb-2 flex items-center gap-4 text-xl font-bold uppercase">
+								<span className="text-primary">
+									<Icon />
+								</span>
+								{title}
 							</h3>
-							<p className="text-muted-foreground">{description}</p>
+							<p className="text-foreground/95 leading-7 font-light md:text-lg md:leading-8">
+								{description}
+							</p>
 						</motion.div>
 					</RevealOnScroll>
 				))}
